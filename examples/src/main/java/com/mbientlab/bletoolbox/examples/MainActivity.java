9883b3dd5dff39e44c8f792d145b17bbe1bab073
etsai <etsai2@gmail.com>
2015-06-02 15:03:43 -0700

    Added example module to test and provide sample code

---
+++ examples/src/main/java/com/mbientlab/bletoolbox/examples/MainActivity.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2015 MbientLab Inc. All rights reserved.
+ */
+
+package com.mbientlab.bletoolbox.examples;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.mbientlab.bletoolbox.scanner.BleDeviceScannerFragment;
+
+import java.util.Locale;
+
+public class MainActivity extends ActionBarActivity implements BleDeviceScannerFragment.ScannerListener {
+    private final static int REQUEST_ENABLE_BT= 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BluetoothAdapter btAdapter= ((BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE)).getAdapter();
+
+        if (btAdapter == null) {
+            new AlertDialog.Builder(this).setTitle(R.string.error_title)
+                    .setMessage(R.string.error_no_bluetooth)
+                    .setCancelable(false)
+                    .setPositiveButton(R.string.label_ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            MainActivity.this.finish();
+                        }
+                    })
+                    .create()
+                    .show();
+        } else if (!btAdapter.isEnabled()) {
+            final Intent enableIntent= new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode) {
+            case REQUEST_ENABLE_BT:
+                if (resultCode == Activity.RESULT_CANCELED) {
+                    finish();
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showBleScan(View v) {
+        new BleDeviceScannerFragment().show(getFragmentManager(), "ble_scanner_fragment");
+    }
+
+    @Override
+    public void btDeviceSelected(BluetoothDevice device) {
+        Toast.makeText(this, String.format(Locale.US, "Selected device: %s", device.getAddress()), Toast.LENGTH_LONG).show();
+    }
+}
